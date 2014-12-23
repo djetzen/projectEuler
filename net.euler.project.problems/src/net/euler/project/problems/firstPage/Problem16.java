d5ac9db09eccbaf9c038998858824a1e92f06e61
Dominik Jetzen <jetzen@itemis.de>
2014-12-23 09:02:35 +0100

    added problem 16 - sum of all the digits of 2^1000

---
+++ net.euler.project.problems/src/net/euler/project/problems/firstPage/Problem16.java
@@ -0,0 +1,25 @@
+package net.euler.project.problems.firstPage;
+
+/**
+ * Created by jetzen on 23.12.14.
+ */
+
+import java.math.BigInteger;
+
+/**
+ * 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+ * What is the sum of the digits of the number 21000?
+ */
+public class Problem16 {
+    public static void main(String[] args) {
+        BigInteger bigInt=new BigInteger("2");
+        BigInteger powed=bigInt.pow(1000);
+        String values=powed.toString();
+        long sum=0;
+        for(int i=0;i<values.length();i++){
+            sum+=Long.parseLong(values.charAt(i)+"");
+        }
+        System.out.println("Number is: "+powed);
+        System.out.println(" and the sum of the digits is: "+sum);
+    }
+}
